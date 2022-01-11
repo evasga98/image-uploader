f073efe674a8c02b97070e76f9d0431144ada59e
evasga98 <evasga98@gmail.com>
2022-01-11 18:59:44 +0100

    align elements of main page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import DropZone from "./dropzone/DropZone";
 
 function App() {
   return (
-    <div>
+    <div className='App'>
         <p className="title">Upload your image</p>
         <p className="subTitle">File should be Jpeg, Png,...</p>
         <div className="content">
           <DropZone />
         </div>
+        <p className="text">Or</p>
+        <button className="button">Choose a File</button>
     </div>
   );
 }
